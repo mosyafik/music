6dc7745b47e388b0a50d78a241f0bcb94d6c1ad3
mosyafik <63044745+mosyafik@users.noreply.github.com>
2020-09-10 15:06:39 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# uptimerobot
-Optional
+# uptime buat bot
---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["http://odm-project.glitch.me"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
