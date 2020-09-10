f6e657108984758e0fbcedf9a6ae161399fcf2cc
mosyafik <63044745+mosyafik@users.noreply.github.com>
2020-09-10 15:08:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["http://odm-project.glitch.me"]
+const urls = ["https://music-odm.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
